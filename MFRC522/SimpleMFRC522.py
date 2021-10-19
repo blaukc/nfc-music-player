62542341ae66ef6187a30841a72bd3fe991fa5d7
blaukc <brandonlau1@hotmail.com>
2021-10-19 16:00:12 +0800

    changed trailer block to 15 for read/write

--- MFRC522/SimpleMFRC522.py
+++ MFRC522/SimpleMFRC522.py
@@ -44,7 +44,7 @@ class SimpleMFRC522:
         return None, None
     id = self.uid_to_num(uid)
     self.READER.MFRC522_SelectTag(uid)
-    status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 19, self.KEY, uid)
+    status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 15, self.KEY, uid)
     data = []
     text_read = ''
     if status == self.READER.MI_OK:
@@ -72,7 +72,7 @@ class SimpleMFRC522:
           return None, None
       id = self.uid_to_num(uid)
       self.READER.MFRC522_SelectTag(uid)
-      status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
+      status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 15, self.KEY, uid)
       self.READER.MFRC522_Read(11)
       if status == self.READER.MI_OK:
           data = bytearray()
