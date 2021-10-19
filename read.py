635adfba1ffd90ef12cfcd766d9c2af3675d49e3
blaukc <brandonlau1@hotmail.com>
2021-10-19 15:33:48 +0800

    testing changing blocks being used for read/write

--- read.py
+++ read.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
+from MFRC522 import SimpleMFRC522
 
-reader = SimpleMFRC522()
+reader = SimpleMFRC522([8, 9, 10, 11, 12, 13])
 
 try:
         id, text = reader.read()
